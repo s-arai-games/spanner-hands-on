43d41ecde799390afbbcf8e1f9ab4f73309e8cc6
s-arai-games <s-arai@serialgames.co.jp>
2018-09-20 15:21:34 +0900

    lombokを削除

--- src/main/java/mapper/User.java
+++ src/main/java/mapper/User.java
@@ -2,15 +2,6 @@ package mapper;
 
 import com.google.cloud.spanner.ResultSet;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
 public class User {
 
   public User(ResultSet resultSet){
@@ -18,7 +9,26 @@ public class User {
     this.setName(resultSet.isNull("name") ? null : resultSet.getString("name"));
   }
 
+  public User(long userId, String name) {
+    this.setUserId(userId);
+    this.setName(name);
+  }
+
   private long userId;
   private String name;
+
+  public long getUserId() {
+    return this.userId;
+  }
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
 
 }
--- src/main/java/mapper/UserFavorite.java
+++ src/main/java/mapper/UserFavorite.java
@@ -2,21 +2,18 @@ package mapper;
 
 import com.google.cloud.spanner.ResultSet;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
 public class UserFavorite {
 
   public UserFavorite(ResultSet resultSet){
     this.setUserId(resultSet.getLong("user_id"));
     this.setFavoriteId(resultSet.getLong("favorite_id"));
     this.setFavoriteThing(resultSet.isNull("favorite_thing") ? null : resultSet.getString("favorite_thing"));
+  }
+
+  public UserFavorite(long userId, long favoriteId, String favoriteThing){
+    this.setUserId(userId);
+    this.setFavoriteId(favoriteId);
+    this.setFavoriteThing(favoriteThing);
   }
 
 //  public UserFavorite(long userId, long favoriteId){
@@ -27,5 +24,26 @@ public class UserFavorite {
   private long userId;
   private long favoriteId;
   private String favoriteThing;
+
+  public long getUserId() {
+    return this.userId;
+  }
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  public long getFavoriteId() {
+    return this.favoriteId;
+  }
+  public void setFavoriteId(long favoriteId) {
+    this.favoriteId = favoriteId;
+  }
+
+  public String getFavoriteThing() {
+    return this.favoriteThing;
+  }
+  public void setFavoriteThing(String favoriteThing) {
+    this.favoriteThing = favoriteThing;
+  }
 
 }
